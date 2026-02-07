d359c26ecde788e0f86e185bc0553a704457be97
Andrii Hrudlevskyi <bgry20@gmail@gmail.com>
2026-02-07 12:37:37 +0100

    start clean

---
+++ main/main/Source.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h> // Потрібно для strlen (довжина рядка)
+
+int main() {
+    char secret_word[20];
+    int i;
+    // strlen рахує скільки букв у слові (щоб не бігти по порожніх клітинках)
+    int length;
+
+    printf("Enter word to encrypt: ");
+    scanf("%19s", secret_word);
+
+    // 1. Дізнаємось довжину слова (наприклад, "halo" = 4)
+    length = strlen(secret_word);
+
+    printf("\nOriginal: %s\n", secret_word);
+
+    // 2. Бігаємо по кожній букві і змінюємо її
+    for (i = 0; i < length; i++) {
+
+        // Магія тут: беремо букву і додаємо 1
+        secret_word[i] = secret_word[i] + 1;
+    }
+
+    printf("Encrypted: %s\n", secret_word);
+
+    return 0;
+}
